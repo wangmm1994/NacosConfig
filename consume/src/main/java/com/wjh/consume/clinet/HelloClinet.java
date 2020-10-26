d9c3c7b56b73de16e8e97f327811e2c10811f488
wangmm1994 <769669064@qq.com>
2020-10-26 15:15:15 +0800

    Update HelloClinet.java

--- consume/src/main/java/com/wjh/consume/clinet/HelloClinet.java
+++ consume/src/main/java/com/wjh/consume/clinet/HelloClinet.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "producer")
+@FeignClient(value = "producers")
 public interface HelloClinet {
     @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
     String echo(@PathVariable String string);
