022a57237ea28356c896eabffaa63f14e5cecd7a
wangmm1994 <769669064@qq.com>
2020-11-09 11:04:39 +0800

    Update HelloController.java

--- consume/src/main/java/com/wjh/consume/controller/HelloController.java
+++ consume/src/main/java/com/wjh/consume/controller/HelloController.java
@@ -15,7 +15,7 @@ public class HelloController {
 
     @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
     public String echo(@PathVariable String string) {
-        System.out.println("consume...113322.");
+        System.out.println("consume...11332112.");
         return helloClinet.echo(string);
     }
 
