78a40402906dfe67388b93d36aecf41d1b7df491
wangmm1994 <769669064@qq.com>
2020-12-23 14:31:38 +0800

    Update HelloController.java

--- producer/src/main/java/com/wjh/producer/controller/HelloController.java
+++ producer/src/main/java/com/wjh/producer/controller/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
 
     @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
     public String echo(@PathVariable String string) {
-        System.out.println("produ-08c");
+        System.out.println("produ-08");
         return  "Hello Nacos Discovery121， " + string;
     }
 }
