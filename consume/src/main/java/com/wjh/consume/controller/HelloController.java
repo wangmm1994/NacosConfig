8a9a23898ff3ce414468c2b6f7989cb2124e7655
wangmm1994 <769669064@qq.com>
2022-08-24 22:05:51 +0800

    Update HelloController.java

--- consume/src/main/java/com/wjh/consume/controller/HelloController.java
+++ consume/src/main/java/com/wjh/consume/controller/HelloController.java
@@ -13,7 +13,7 @@ public class HelloController {
     @Autowired
     private HelloClinet helloClinet;
 
-    @RequestMapping(value = "/echooo/{string}", method = RequestMethod.GET)
+    @RequestMapping(value = "/echoooy/{string}", method = RequestMethod.GET)
     public String echo(@PathVariable String string) {
         System.out.println("https://mp.weixin.qq.com/s?__biz=MjM5ODYxMDA5OQ==&mid=2651961368&idx=1&sn=82a59f41332e11a29c5759248bc1ba17&chksm=bd2d0dc48a5a84d293f5999760b994cee9b7e20e240c04d0ed442e139f84ebacf608d51f4342&scene=21#wechat_redirect");
         return helloClinet.echo(string);
